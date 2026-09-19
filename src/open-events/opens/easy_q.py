8f70a15afbfc875b1d65f0df5d06d7a185feba7b
Mat1020 <matias.tajan@gmail.com>
2026-09-19 17:23:58 -0400

    Updated function

--- src/open-events/opens/easy_q.py
+++ src/open-events/opens/easy_q.py
@@ -53,19 +53,19 @@ def normal_questions(score, questions_remaining):
     return score
 
 def random_questions(score, questions_remaining):
-
     easy_questions_dict = {
-        "What is 'Hello' in French? ": "bonjour",
-        "What is 'please' in French? ": "s'il vous plaît",
-        "What is 'thanks' in French? ": "merci",
-        "What is 'you're welcome' in French? ": "de rein",
-        "What is 'sorry' in French (formal)? ": "pardon",
-        "What is 'goodbye' in French? ": "au revoir"
+        "What is 'Hello' in French? ": ["bonjour", "au revoir", "pardon", "merci"],
+        "What is 'please' in French? ": ["s'il vous plaît", "au revoir", "pardon", "merci"],
+        "What is 'thanks' in French? ": ["merci", "au revoir", "pardon", "s'il vous plaît"],
+        "What is 'you're welcome' in French? ": ["de rien", "bonjour", "pardon", "merci"],
+        "What is 'sorry' in French (formal)? ": ["pardon", "au revoir", "de rien", "merci"],
+        "What is 'goodbye' in French? ": ["au revoir", "bonjour", "pardon", "merci"]
     }
 
     random_list = []
-    for q, a in easy_questions_dict.items():
-        random_list.append((q, a))
+
+    for random_question, random_answer in easy_questions_dict.items():
+        random_list.append((random_question, random_answer))
 
     random.shuffle(random_list)
     fresh_questions_dict = dict()
@@ -73,19 +73,44 @@ def random_questions(score, questions_remaining):
     for item in random_list:
         fresh_questions_dict[item[0]] = item[1]
 
-    for q, a in fresh_questions_dict.items():
-        sleep(1)
-        print()
+    for random_question, random_answer in fresh_questions_dict.items():
+        sleep(0.5)
+
         questions_remaining += 1
-        print(f"{questions_remaining}Q:")
+        print(f"\n{questions_remaining}Q:")
 
-        answer = input(q)
+        correct_answer = random_answer[0] # The first answer is the correct
 
-        if answer.lower() == a:
-            print("Correct!")
-            score += 1
+        answer_choices = random_answer.copy()
+        random.shuffle(answer_choices)
+
+        print(random_question)
+
+        print(f"A. {answer_choices[0].capitalize()}")
+        print(f"B. {answer_choices[1].capitalize()}")
+        print(f"C. {answer_choices[2].capitalize()}")
+        print(f"D. {answer_choices[3].capitalize()}")
+
+        answer = input("Your answer: ").lower()
+
+        answer_indexes = {
+            "a": 0,
+            "b": 1,
+            "c": 2,
+            "d": 3
+        }
+
+        if answer in answer_indexes:
+            selected_answer = answer_choices[answer_indexes[answer]]
+
+            if selected_answer == correct_answer:
+                print("Correct!")
+                score += 1
+            else:
+                print("Incorrect.")
         else:
-            print("Incorrect.")
+            print("Invalid answer. Please enter A, B, C, or D.")
 
     print()
+
     return score
